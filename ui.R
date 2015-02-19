b3ab643aebfe6cd8a40776808e35006fdbfd7bff
sbstolk <sb.stolk@gmail.com>
2015-02-19 22:12:02 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,27 +1,13 @@
-library(shiny)
 shinyUI(pageWithSidebar(
-headerPanel("Central Limit Theorem Demo"),
-sidebarPanel(
-sliderInput("n",
-"Number of samples:",
-min = 1,
-max = 2500,
-value = 100),
-sliderInput("bins",
-"Number of bins:",
-min = 1,
-max = 100,
-value = 20),
-withMathJax(),
-uiOutput('population_mean'),
-uiOutput('sample_means_mean'),
-uiOutput('population_sd'),
-uiOutput('sample_means_mean_sd'),
-h4("Documentation"),
-helpText("The app allows you to see the Central Limit Theorem in action. This is a key concept for statistics & statistical modeling."),
-helpText("Check out how the values change as you alter the number of samples and the number of bins."),
-mainPanel(
-plotOutput("distPlot", height = "300px", width="80%"),
-plotOutput("sampleDistPlot", height = "300px", width="80%")
-)
+  headerPanel('Iris k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris)),
+    selectInput('ycol', 'Y Variable', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 5)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
 ))
